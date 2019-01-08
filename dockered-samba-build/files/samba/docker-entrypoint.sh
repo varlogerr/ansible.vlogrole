7809da9b286ddf4800e4d5eb70e254fec95bf71c
Varlog <varlog.err@gmail.com>
2019-01-08 19:41:57 +0200

    WiP fixes

--- dockered-samba-build/files/samba/docker-entrypoint.sh
+++ dockered-samba-build/files/samba/docker-entrypoint.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
 
+# execute commands
+while read var_name; do
+  eval_cmd=${!var_name}
+  
+  eval "${eval_cmd}"
+done <<< "$(compgen -e | grep ^EVAL_CMD_)"
 
+# create share configurations
+while read var_name; do
+  share_name=${var_name:12}
+  share_conf=${!var_name}
+  custom_conf_path="${SAMBA_CUSTOM_CONF_DIR}/${share_name}.conf"
+
+  echo "[${share_name}]\n${share_conf}" | tee "${custom_conf_path}"
+  echo "include = ${custom_conf_path}" | tee -a ${SAMBA_CONF}
+done <<< "$(compgen -e | grep ^SAMBA_SHARE_)"
+
+service nmbd start
 smbd -FS < /dev/null
